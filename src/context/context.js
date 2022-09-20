74c72ab8190a2bdade7d4a78b832004029b15037
Bidz42 <abid97r@gmail.com>
2022-09-20 09:36:00 +0100

    New refactor

---
+++ src/context/context.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+const API_URL='http://localhost:5005';
+const AuthContext = createContext();
+
+const ApiContext = (props) => {
+    const navigate = useNavigate();
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const [toggle, setToggle] = useState(false);
+    //const [images, setImages] = useState([]);
+
+    const setToken = (token) => {
+        localStorage.setItem('authToken', token);
+    }
+
+    const authenticateUser = async () =>{
+        const storeToken = localStorage.getItem('authToken');
+
+        if(storeToken) {
+            axios.get(`${API_URL}/auth/verify`, {headers: {Authorization: `Bearer ${storeToken}`}})
+            .then((response) => {
+                const user = response.data;
+                setIsLoggedIn(true);
+                setIsLoading(false);
+                setUser(user);
+            })
+            .catch((error) => {
+                setIsLoggedIn(false);
+                setIsLoading(false);
+                setUser(null);
+            });
+        } else {
+            setIsLoggedIn(false);
+            setIsLoading(false);
+            setUser(null);
+        }
+    };
+
+    const removeToken = () => {
+        localStorage.removeItem("authToken");
+    };
+
+    const logOutUser = () => {
+        removeToken();
+        authenticateUser();
+        navigate('/login');
+    };
+      
+      useEffect(() => { 
+        authenticateUser();
+      },[]);
+
+return (
+    <AuthContext.Provider value={{setToken, authenticateUser, logOutUser, user, isLoading, isLoggedIn, toggle, setToggle}}>
+        {props.children}
+    </AuthContext.Provider>
+    )
+};
+
+export { ApiContext, AuthContext };
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,83 @@
+import React, {useState, useContext} from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import {AuthContext} from '../context/context';
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [setErrorMessage] = useState(undefined);
+  const { setToken, authenticateUser } = useContext(AuthContext);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+      const requestBody = { email, password };
+    
+      axios
+          .post(`http://localhost:5005/auth/login`, requestBody)
+          .then((response) => {
+            console.log("JWT token", response.data.authToken);
+            setToken(response.data.authToken);
+            authenticateUser();
+            navigate("/");
+          })
+          .catch((error) => {
+            const errorDescription = error.response.data.message;
+            setErrorMessage(errorDescription);
+          });
+      };
+
+  return (
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-sky-400 rounded-md shadow-xl shadow-gray-600/40 ring-2 ring-gray-600 lg:max-w-xl bg-scroll">
+
+                <h1 className="text-3xl text-gray-50 font-amita font-semibold text-center  uppercase decoration-wavy">
+                    Create an account
+                </h1>
+
+                <form className="mt-6" onSubmit={handleLoginSubmit}>
+                    
+                    <div className="mb-2">
+                        <label for="email" className="block text-base text-gray-50 font-amita font-semibold">
+                            EMAIL
+                        </label>
+                        <input type="email"
+                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                            placeholder='Enter your email'
+                            onChange={(e) => setEmail(e.target.value)} value={email}
+                            />
+                        <p className='text-xs font-medium tracking-widest text-gray-50 font-lobster mb-5'>We'll never share your email with anyone else. </p>
+                    </div>
+
+                    <div className="mb-2">
+                        <label
+                            for="password"
+                            className="block text-base tracking-widest text-gray-50 font-amita font-semibold"
+                        >
+                            PASSWORD
+                        </label>
+                        <input
+                            type="password"
+                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"g
+                            placeholder='Enter your password'
+                            onChange={(e) => setPassword(e.target.value)} value={password}
+                            />
+                    </div>
+
+                    <div className="mt-6">
+                        <button className="w-full bg-gray-50 px-4 py-2 font-amita text-gray-700 text-lg font-black tracking-widest transition-colors duration-200 transform rounded-md hover:bg-slate-300 focus:outline-none focus:bg-slate-300"
+                        type="submit">
+                            JOIN SHH-AREA
+                        </button>
+                    </div>                
+
+                </form>
+            </div>
+        </div>
+  )
+}
+
+export default LoginPage
