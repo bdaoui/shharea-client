d9509394f0d13a10413e3a6ddcdf3e1b4f5b36d3
Bidz42 <abid97r@gmail.com>
2022-09-20 10:45:14 +0100

    change

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -30,45 +30,39 @@ const LoginPage = () => {
       };
 
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-sky-400 rounded-md shadow-xl shadow-gray-600/40 ring-2 ring-gray-600 lg:max-w-xl bg-scroll">
+    <div>
+            <div>
 
-                <h1 className="text-3xl text-gray-50 font-amita font-semibold text-center  uppercase decoration-wavy">
+                <h1>
                     Create an account
                 </h1>
 
-                <form className="mt-6" onSubmit={handleLoginSubmit}>
+                <form onSubmit={handleLoginSubmit}>
                     
-                    <div className="mb-2">
-                        <label for="email" className="block text-base text-gray-50 font-amita font-semibold">
+                    <div>
+                        <label for="email">
                             EMAIL
                         </label>
                         <input type="email"
-                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             placeholder='Enter your email'
                             onChange={(e) => setEmail(e.target.value)} value={email}
                             />
-                        <p className='text-xs font-medium tracking-widest text-gray-50 font-lobster mb-5'>We'll never share your email with anyone else. </p>
+                        <p>We'll never share your email with anyone else. </p>
                     </div>
 
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-base tracking-widest text-gray-50 font-amita font-semibold"
-                        >
+                    <div>
+                        <label for="password">
                             PASSWORD
                         </label>
                         <input
                             type="password"
-                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"g
                             placeholder='Enter your password'
                             onChange={(e) => setPassword(e.target.value)} value={password}
                             />
                     </div>
 
                     <div className="mt-6">
-                        <button className="w-full bg-gray-50 px-4 py-2 font-amita text-gray-700 text-lg font-black tracking-widest transition-colors duration-200 transform rounded-md hover:bg-slate-300 focus:outline-none focus:bg-slate-300"
-                        type="submit">
+                        <button type="submit">
                             JOIN SHH-AREA
                         </button>
                     </div>                
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -24,70 +24,61 @@ const SignUpPage = () => {
     }
 
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-sky-400 rounded-md shadow-xl shadow-gray-600/40 ring-2 ring-gray-600 lg:max-w-xl bg-scroll">
+    <div>
+            <div>
 
-                <h1 className="text-3xl text-gray-50 font-amita font-semibold text-center  uppercase decoration-wavy">
+                <h1>
                     Create an account
                 </h1>
 
-                <form className="mt-6" onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
 
-                    <div className="mb-2">
+                    <div>
                         <label
-                            for="username"
-                            className="block text-base text-gray-50 font-amita font-semibold">
+                            for="username">
                             USERNAME
                         </label>
                         <input
                             type="text"
-                            className="block font-amita w-full px-4 py-2 mt-2 mb-5 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             placeholder='Enter your username'
                             onChange={(e) => setUsername(e.target.value)} value={username}
                             />
                     </div>
 
-                    <div className="mb-2">
-                        <label for="name" className="block text-base text-gray-50 font-amita font-semibold">
+                    <div>
+                        <label for="name">
                             name
                         </label>
                         <input type="name"
-                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             placeholder='Enter your name'
                             onChange={(e) => setName(e.target.value)} value={name}
                             />
                     </div>
                     
-                    <div className="mb-2">
-                        <label for="email" className="block text-base text-gray-50 font-amita font-semibold">
+                    <div>
+                        <label for="email">
                             EMAIL
                         </label>
                         <input type="email"
-                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             placeholder='Enter your email'
                             onChange={(e) => setEmail(e.target.value)} value={email}
                             />
-                        <p className='text-xs font-medium tracking-widest text-gray-50 font-lobster mb-5'>We'll never share your email with anyone else. </p>
+                        <p >We'll never share your email with anyone else. </p>
                     </div>
 
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-base tracking-widest text-gray-50 font-amita font-semibold"
-                        >
+                    <div>
+                        <label for="password">
                             PASSWORD
                         </label>
                         <input
                             type="password"
-                            className="block w-full px-4 py-2 mt-2 mb-2 tracking-widest font-amita text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"g
                             placeholder='Enter your password'
                             onChange={(e) => setPassword(e.target.value)} value={password}
                             />
                     </div>
 
-                    <div className="mt-6">
-                        <button className="w-full bg-gray-50 px-4 py-2 font-amita text-gray-700 text-lg font-black tracking-widest transition-colors duration-200 transform rounded-md hover:bg-slate-300 focus:outline-none focus:bg-slate-300"
-                        type="submit">
+                    <div >
+                        <button type="submit">
                             JOIN SHH-AREA
                         </button>
                     </div>                
