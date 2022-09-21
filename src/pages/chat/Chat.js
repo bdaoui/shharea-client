9808de36fbe6e6a08c3e10e9197271570a6d5dbe
Bidz42 <abid97r@gmail.com>
2022-09-21 22:02:38 +0100

    chat

---
+++ src/pages/chat/Chat.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+const Chat = () => {
+
+const {id} = useParams()
+    
+useEffect(() => {
+    const domain = 'https://shharea.daily.co/'
+    axios
+        .get(`http://localhost:5005/chat/room/${id}`)
+        .then((res) => {
+            if (res.status === 200){
+                const script = document.createElement("script");
+                script.innerHTML =  `window.DailyIframe.createFrame({
+                    iframeStyle: {
+                        position: 'fixed',
+                        top: 20,
+                        width: '100%',
+                        height: '80%',
+                    },
+                    showLeaveButton: true,
+                })
+                .join({
+                    url: '${domain}${id}',
+                });`;
+                document.body.appendChild(script);
+            }
+        })
+        .catch(err => console.log(err));
+}, [id]);
+
+  return (
+   <>
+   </>
+  )
+}
+
+export default Chat
---
+++ src/pages/chat/JoinChat.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const JoinChat = () => {
+
+const [room, setRoom] = useState(null);
+
+const handleJoin = (e) => {
+    e.preventDefault();
+    window.location.assign(`/room/${room}`)
+}
+
+    return (
+    <div>
+        <span>Chat Rooms</span>
+        <p>Enter the room you want to join</p>
+        <input name="room" label="room" onChange={(e) => setRoom(e.target.value)} /> 
+        <button onClick={handleJoin}>Join Room</button>
+    </div>
+    )
+}
+
+export default JoinChat
