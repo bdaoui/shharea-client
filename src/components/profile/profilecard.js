428cb7b3b927c77572db443c94f2b27ba160b602
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-23 12:30:32 +0200

    profile edit

--- src/components/profile/profilecard.js
+++ src/components/profile/profilecard.js
@@ -1,122 +1,137 @@
-import React, {useContext, useState, useEffect} from 'react'
-import {AuthContext} from '../../context/context';
-import axios from 'axios'
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/context";
+import axios from "axios";
 
+const ProfileCard = ({ id }) => {
+  const { user, toggle, setToggle } = useContext(AuthContext);
+  
+  const userId = user?._id;
+  const [details, setDetails] = useState({});
+  const [friend, setFriend] = useState(false);
+  
+  const [email, setEmail] = useState(user?.email);
+  const [username, setUsername] = useState(user?.username);
+  const [info, setInfo] = useState(user?.info);
+  const [location, setLocation] = useState(user?.location);
 
-const ProfileCard = ({id}) => {
-const { user, toggle, setToggle } = useContext(AuthContext);
-const [location, setLocation] = useState("")
-const [info, setInfo] = useState("")
-const userId = user?._id;
-const [details, setDetails] = useState({})
-const [friend, setFriend] = useState(false)
-
-useEffect(()=>{
+  useEffect(() => {
     axios
-    .get( `http://localhost:5005/user/${id}/details` )
-    .then(response => setDetails(response.data))
-    .catch(err => console.log(err));
+      .get(`http://localhost:5005/user/${id}/details`)
+      .then((response) => setDetails(response.data))
+      .catch((err) => console.log(err));
+
     // eslint-disable-next-line
-}, [])
+  }, []);
 
-
-
-
-const handleProfile = (e) =>{
+  const handleProfile =  (e) => {
     e.preventDefault();
+
+    
+    console.log("info ", info, "location ", location)    
+
     axios
-        .post("http://localhost:5005/user/profile", {userId, location, info})
-        .then(response => console.log(response.data))
-        .catch(err => console.log(err))
-}
+      .post("http://localhost:5005/user/profile", { userId, location, info, email, username })
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err));
+  };
 
-
-
-const handleFriendship = async () => {
-    await friend ? setFriend(false) : setFriend(true);
-    try{
-       const friendsResponse = await axios.post(`http://localhost:5005/user/${id}/friends`, {userId})
-       console.log(friendsResponse);
+  const handleFriendship = async () => {
+    (await friend) ? setFriend(false) : setFriend(true);
+    try {
+      const friendsResponse = await axios.post(
+        `http://localhost:5005/user/${id}/friends`,
+        { userId }
+      );
+      console.log(friendsResponse);
+    } catch (error) {
+      console.log(error);
     }
-   catch(error) {console.log(error)}
-}
+  };
 
+  // Show and Hide Edit Form
 
-
-// Show and Hide Edit Form
-
-const toggler = () => {
-    if(toggle === false){
-        setToggle(true)
+  const toggler = () => {
+    if (toggle === false) {
+      setToggle(true);
     } else {
-        setToggle(false)
+      setToggle(false);
     }
-}
+  };
+
 
 
   return (
-    <div>  
-        
-    
-        <div>
-            <h1>Name: <span>{details?.name}</span> </h1> 
-            <h2>Location: <span>{details?.location}</span></h2>
-            <h2>Info:</h2>
-            <p>{details?.info}</p>
+    <div>
+      <div>
+        <h1>
+          Name: <span>{details?.name}</span>{" "}
+        </h1>
+        <h2>
+          Location: <span>{details?.location}</span>
+        </h2>
+        <h2>Info:</h2>
+        <p>{details?.info}</p>
 
-            {userId === id && 
-                <button onClick={toggler}>Edit</button>
-            }
-            
-            {/* Shows if not current user */}
-            {!(userId === id) && 
-                <button onClick={handleFriendship}>Friend Request</button>
-            }
-            
+        {userId === id && <button onClick={toggler}>Edit</button>}
 
-        </div>
+        {/* Shows if not current user, FRIEND REQUEST*/}
+        {!(userId === id) && (
+          <button onClick={handleFriendship}>Friend Request</button>
+        )}
+      </div>
 
+      {/* Edit User Information Section*/}
 
-    { toggle &&
-    <>
-    <form onSubmit={handleProfile}>
-        <div>
-            <label
-                for="location">
-                Location
-            </label>
-            <input
+      {toggle && (
+        <>
+          <form onSubmit={handleProfile}>
+            <div>
+              <label for="location">Location</label>
+              <input
                 type="text"
-                placeholder='Enter your location'
-                onChange={(e) => setLocation(e.target.value)} value={location}
-                />
-        </div>
+                placeholder={details.location}
+                onChange={(e) => setLocation(e.target.value)}
+                value={location}
+              />
+            </div>
 
-        
-
-        <div>
-            <label for="info">
-                Info
-            </label>
-            <textarea
+            <div>
+              <label for="info">Info</label>
+              <textarea
                 type="text"
-                placeholder='Enter anything you want to share'
-                onChange={(e) => setInfo(e.target.value)} value={info}
-                />
-        </div>
+                placeholder={details.info}
+                onChange={(e) => setInfo(e.target.value)}
+                value={info}
+              />
+            </div>
 
-        <div >
-            <button type="submit">
-                Update profile
-            </button>
-        </div>                
+            <div>
+              <label for="username">username</label>
+              <input
+                type="text"
+                placeholder={details.username}
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+              />
+            </div> 
+            <div>
+              <label for="email">email</label>
+              <input
+                type="text"
+                placeholder={details.email}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div> 
 
-    </form>
-    </>
-    }
+            <div>
+              <button type="submit">Update profile</button>
+            </div>
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
 
- </div>
-  )
-}
-
-export default ProfileCard
+export default ProfileCard;
