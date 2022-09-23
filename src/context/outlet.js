83dca78235733d9f541ffe1fb32c1a37225f1691
Bidz42 <abid97r@gmail.com>
2022-09-23 18:17:52 +0100

    secojnd thoughts mui

--- src/context/outlet.js
+++ src/context/outlet.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import NavBar from '../components/navbar/navbar';
-import ResponsiveAppBar from '../components/navbar/responsiveappbar';
 
 function OutletComponent() {
   return (
     <div>
-        <ResponsiveAppBar />
+        <NavBar />
         <Outlet />
 
     </div>
