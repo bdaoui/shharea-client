f3efd7b58f8749312fa54e279f2d414afc16918a
Bidz42 <abid97r@gmail.com>
2022-09-25 20:53:16 +0100

    feed done

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -9,10 +9,13 @@ import Link from '@mui/material/Link';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import CommentIcon from '@mui/icons-material/Comment';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {theme} from '../../index'
 
 const Feed = () => {
 
 const { images, setImages } = useContext(AuthContext);
+
 
 useEffect(() => {
    axios
@@ -22,20 +25,25 @@ useEffect(() => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
 }, [])
 
+
+const breakpoint = useMediaQuery(theme.breakpoints.down('sm' && 'md'));
+
   return (
     <div>
 
 {images && 
        
-  <ImageList xs={12} md={12} lg={12} sx={{ width: '100%', height: '100%' }} variant="masonry" cols={4} gap={2} >
+  <ImageList sx={{ width: '98%', height: '100%', margin:'auto' }} variant="masonry" cols={breakpoint ? 1 : 4 } gap={2} >
     {images?.map((item) => (
     <ImageListItem key={item._id}>
       <img src={`${item.imageUrl}?w=500&fit=crop&auto=format`} srcSet={`${item.imageUrl}?w=500&fit=crop&auto=format&dpr=2 2x`} alt={item.name} loading="lazy" />
         <Link href={`/home/image/${item._id}`}>
         <ImageListItemBar title={item?.name} subtitle={`Posted by @${item?.owner?.username}`} actionIcon={
           <IconButton sx={{ color: 'rgba(255, 255, 255, 0.54)' }} aria-label={`info about ${item.name}`}>
+            
             <FavoriteIcon sx={{mr:1}} /> 
             <CommentIcon />
+            
           </IconButton>}/></Link>
     </ImageListItem>))}
   </ImageList>
--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 import React, {useContext, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import {AuthContext} from '../../context/context';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+import {Box, TextField} from '@mui/material'
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 
 const Upload = () => {
@@ -9,9 +15,11 @@ const Upload = () => {
     const navigate = useNavigate();
     const [name, setName] = useState("");
     const [tags, setTags] = useState([]);
+    const [toggle, setToggle] = useState(false)
     const userId=user?._id
 
     const handleUpload = (e) => {
+    console.log('here', e?.target.image)
     e.preventDefault();
     const tagsArray = [...tags].join('').split(',')
     const uploadData = new FormData();
@@ -19,50 +27,113 @@ const Upload = () => {
     uploadData.append("name", name);
     uploadData.append("owner", userId );
     uploadData.append("tags", JSON.stringify(tagsArray));
-    
+     
     axios
         .post('http://localhost:5005/home/upload', uploadData)
         .then((response) => console.log(response))
         .catch((err) => console.log("Error while uploading the file: ", err));
         navigate('/home')
     };
+    
+    const toggler = () => {
+        if (toggle === false) {
+          setToggle(true);
+        } else {
+          setToggle(false);
+        }
+      };
 
-
+     
   return (
     <div>
 
-      
-        <form onSubmit={handleUpload}>
+{!toggle &&
+        <AddCircleIcon onClick={toggler} sx={{mt:5, mb:5}} color="secondary" />
+}
 
-<div>
-    <label for="name">
-        NAME
-    </label>
-    <input type="name"
-        placeholder='Name your upload' onChange={(e) => setName(e.target.value)} value={name}/>
+ {toggle &&
+ 
+ <>
+       
+<form onSubmit={handleUpload} style={{display: 'flex', alignItems: 'center', justifyItems: 'center', flexDirection: 'column', marginTop: '15px'}}>
+<TextField
+            type="name"
+            margin="normal"
+            required
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            onChange={(e) => setName(e.target.value)} 
+            value={name}
+            style={{width:'40%'}}
+            variant="filled"
+            color="secondary"
+          /> 
+         <div style={{display: 'flex'}}>
+          <Button variant="contained" component="label" color="primary" sx={{p:0.5, pt:0, pb:0}}>
+            CHOOSE FILE <input type="file" name="image" hidden/>
+            </Button>
+            <IconButton color="secondary" aria-label="upload picture" component="label">
+            <PhotoCamera />
+            </IconButton>
+         </div>
+           <TextField
+            type="tags"
+            margin="normal"
+            required
+            id="tags"
+            label="Initiate a tag with #, Use commas to seperate tags"
+            name="tags"
+            autoComplete="tags"
+            onChange={(e) => setTags(e.target.value)} 
+            value={tags}
+            style={{width:'40%'}}
+            variant="filled"
+            color="secondary"
+          />
+   
+   <Button variant="contained" component="label" color="primary" sx={{p:0.5, pt:0, pb:0, mb:10}}>
+    UPLOAD TO SHH-AREA
+    <button type="submit" hidden></button>
+    </Button>
+    </ form>
+</>
+  }   
+  
 </div>
-
-<div >
-    <label for="image" >
-      SELECT YOUR IMAGE
-    </label>
-    <input type="file" name="image" />
-</div>
-
-<div >
-    <label for="tags" >
-        #TAGS
-    </label>
-    <textarea type="tags" 
-        placeholder='Initiate a tag with #,
-        Use commas to seperate tags' onChange={(e) => setTags(e.target.value)} value={tags}/>
-</div>
-<button type="submit">Upload Image!!!</button>
-</form>
-
-        </div>
    
   )
 }
 
-export default Upload
+export default Upload 
+
+
+{/* <form onSubmit={handleUpload}>
+
+        <div>
+            <label for="name">
+                NAME
+            </label>
+            <input type="name"
+                placeholder='Name your upload' onChange={(e) => setName(e.target.value)} value={name}/>
+        </div>
+
+        <div >
+            <label for="image" >
+            SELECT YOUR IMAGE
+            </label>
+            <input type="file" name="image" />
+        </div>
+
+        <div >
+            <label for="tags" >
+                #TAGS
+            </label>
+            <textarea type="tags" 
+                placeholder='Initiate a tag with #,
+                Use commas to seperate tags' onChange={(e) => setTags(e.target.value)} value={tags}/>
+        </div>
+        <button type="submit">Upload Image!!!</button>
+        </form> */}
