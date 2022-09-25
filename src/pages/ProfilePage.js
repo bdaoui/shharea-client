adc18a7fb45817f6c46dcb82545de48452ed6bef
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-25 23:35:44 +0200

    working on grid

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,66 +1,74 @@
-import React, {useEffect, useState} from 'react'
-import { useParams, Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
-import ProfileCard from '../components/profile/profilecard';
+import ProfileCard from "../components/profile/profilecard";
+import Grid from "@mui/material/Grid";
 
 const ProfilePage = () => {
-    const {id} = useParams();
-    const [ownImages, setOwnImages] = useState([]);
- 
-    useEffect(() =>{
+  const { id } = useParams();
+  const [ownImages, setOwnImages] = useState([]);
+
+  useEffect(() => {
     axios
-        .get(`http://localhost:5005/user/profile/${id}`)
-        .then(response => setOwnImages(response.data))
-        .catch(err => console.log(err));
-    },[id])    
+      .get(`http://localhost:5005/user/profile/${id}`)
+      .then((response) => setOwnImages(response.data))
+      .catch((err) => console.log(err));
+  }, [id]);
 
-    return (
-    
-    <div>ProfilePage
- 
-    <ProfileCard id={id}/>
+  return (
+    <Grid container>
+      ProfilePage
+      <ProfileCard id={id} />
+      {ownImages &&
+        ownImages?.map((item) => {
+          return (
 
-
-
-    {
-        ownImages && 
-        ownImages?.map( item =>{
-           return (
-          <div key={item?._id}>
-            <img
-              src={item?.imageUrl}
-              alt={item?.name}
-              loading="lazy"
-              style={{
-                display: 'block',
-                width: '25%',
-              }}
-            />
-            <Link to={`/home/image/${item?._id}`}>{item?.name}</Link>
-            
-            <span>{item?.tags}</span>
-
-            <div> 
-              <h2>This Image Has {item?.comments?.length} comments</h2>
-              {item?.comments?.map(comment => {
-                return <p key={comment._id}><span>{comment?.owner?.username} said: </span> {comment.comment}</p>
-              })}
-            </div>
            
-            <div> 
-              <h2>This Image Has {item?.likes?.length} Likes</h2>
-              {item?.likes?.map(like => {
-                return <p key={like._id}><span>{like?.username} liked it </span></p>
-              })}
+            <div key={item?._id}>
+            <Grid >
+              <Link to={`/home/image/${item?._id}`}>{item?.name}</Link>
+              <span>{item?.tags}</span>
+            </Grid>
+
+         
+
+              <img
+                src={item?.imageUrl}
+                alt={item?.name}
+                loading="lazy"
+                style={{
+                  display: "block",
+                  width: "25%",
+                }}
+              />
+
+              <div>
+                <h2>This Image Has {item?.likes?.length} Likes</h2>
+                {item?.likes?.map((like) => {
+                  return (
+                    <p key={like._id}>
+                      <span>{like?.username} liked it </span>
+                    </p>
+                  );
+                })}
+              </div>
+
+              <div>
+                <h2>This Image Has {item?.comments?.length} comments</h2>
+                {item?.comments?.map((comment) => {
+                  return (
+                    <p key={comment._id}>
+                      <span>{comment?.owner?.username} said: </span>{" "}
+                      {comment.comment}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
+          );
+        })}
+    </Grid>
+  );
+};
 
-
-     </div>
-        )})}
-
-    
-    </div>
-  )
-}
-
-export default ProfilePage
+export default ProfilePage;
